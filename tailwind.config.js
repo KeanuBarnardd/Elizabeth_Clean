bbc2c1dd074dbc96fbd4c7144208490d4b30309c
Keanu Barnard <keanu.barnardd@gmail.com>
2023-09-17 07:01:58 +1000

    Updated Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./layout/*.liquid",
-        "./sections/*.liquid",
-        "./snippets/*.liquid",
-        "./templates/customers/*.liquid",
-        "./templates/*.liquid",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*.liquid",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
